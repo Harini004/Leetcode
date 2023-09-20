714822d93839ff5c5bfc56eb3d3cec18356530ac
Harini <45751597+Harini004@users.noreply.github.com>
2023-09-20 14:50:04 +0200

    Time: 107 ms (39.48%), Space: 16.5 MB (85.00%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,25 +1,10 @@
-class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        int[] count = new int[mat.length];
-        int[] res = new int[k];
-        
-        int sol;
-        for(int i = 0;i<mat.length;i++)
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        ans=[]
+        n= len(mat)
+        for i in range(n):
+            ans.append([mat[i].count(1),i])
             
-        {
-            sol =0;
-            for(int j = 0;j<mat[0].length;j++)
-            {
-                if(mat[i][j]==1)
-                    sol++;
-            }
-            count[i] = sol*1000 +i;
-        }
-        Arrays.sort(count);
-        for(int i=0;i<k;i++)
-        {
-            res[i] = count[i]%1000; 
-        }
-        return res;
-    }
-}
+        ans.sort()
+        return [i[1] for i in ans[:k]]
+        
