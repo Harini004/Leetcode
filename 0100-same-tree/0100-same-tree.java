22f5fdb88a5b0ecb4d7f3d4ef3a545ce199d55a1
Harini <45751597+Harini004@users.noreply.github.com>
2024-02-27 21:34:21 +0100

    Time: 0 ms (100.00%), Space: 41.3 MB (11.41%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> q1 = new LinkedList<>();
+        if(p==null && q==null)
+            return true;
+        else if(p==null || q==null)
+            return false;
+        if(p!=null && q!=null)
+        {
+            q1.offer(p);
+            q1.offer(q);
+        }
+        while(!q1.isEmpty())
+        {
+            TreeNode first = q1.poll();
+            TreeNode second = q1.poll();
+            if(first == null && second == null)
+                continue;
+            if(first == null || second == null)
+                return false;
+            if(first.val != second.val)
+                return false;
+            q1.offer(first.left);
+            q1.offer(second.left);
+            q1.offer(first.right);
+            q1.offer(second.right);
+        }
+        return true;
+        
+    }
+}
