36ad3af336db9eccb9ddc9013274ce16c7ca148d
Harini <45751597+Harini004@users.noreply.github.com>
2024-02-28 10:57:45 +0100

    Time: 135 ms (77.47%), Space: 41.4 MB (79.18%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,14 +1,15 @@
 class Solution {
     boolean vis[][];
     public boolean exist(char[][] board, String word) {
-     
+        int r = board.length;
+        int c = board[0].length;
         
-        vis = new boolean[board.length][board[0].length];
-        for(int i = 0;i<board.length;i++)
+        vis = new boolean[r][c];
+        for(int i = 0;i<r;i++)
         {
-            for(int j = 0;j<board[i].length;j++)
+            for(int j = 0;j<c;j++)
             {
-                if((word.charAt(0)==board[i][j]) && search(i,j,0,board,word)  )
+                if(search(i,j,0,board,word) && (word.charAt(0)==board[i][j]))
                     return true;
             }
         }
@@ -16,15 +17,15 @@ class Solution {
     }
     private boolean search(int i,int j, int index, char[][] board, String word)
     {
-         if(index == word.length())
+                if(index == word.length())
             return true;
-        if(  i>=board.length|| i<0 || j>=board[i].length|| j<0||board[i][j]!= word.charAt(index) ||vis[i][j] )
+        if(  i>=board.length||i<0 || j>=board[i].length||j<0 || word.charAt(index)!=board[i][j]||vis[i][j] )
             return false;
         
-       
+
         vis[i][j] = true;
         if(
-            search(i-1,j,index+1,board,word) ||  search(i+1,j,index+1,board,word) ||  search(i,j-1,index+1,board,word) ||  search(i,j+1,index+1,board,word)
+            search(i+1,j,index+1,board,word) ||  search(i-1,j,index+1,board,word) ||  search(i,j+1,index+1,board,word) ||  search(i,j-1,index+1,board,word)
         )
             return true;
         vis[i][j] = false;
