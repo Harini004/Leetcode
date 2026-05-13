1d8a42abefaa6dcf8d33962da0127436c75c2c57
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-13 10:27:20 +0200

    Time: 276 ms (53.46%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select (
-select distinct salary from (
-select salary, dense_rank() over (order by salary desc) as salary_rank from Employee)
-as ranked_sal where salary_rank = 2 )as SecondHighestSalary 
+
+with highest_sal as (
+select id, salary, dense_rank() over (order by salary desc) as salary_rank from Employee)
+select (select salary from highest_sal where salary_rank = 2 limit 1 ) as SecondHighestSalary;
