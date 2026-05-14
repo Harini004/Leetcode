1a37e6f978f6256af266083825f9fbca0ddceeee
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-14 11:54:25 +0200

    .sql Easy 1068-product-sales-analysis-i 1298 ms (37.36%) 0B (100%)

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select p.product_name, s.year, s.price from Sales s
-join
-Product p on p.product_id = s.product_id
+select product_name, year, price from 
+sales s inner join product p
+on s.product_id = p.product_id
