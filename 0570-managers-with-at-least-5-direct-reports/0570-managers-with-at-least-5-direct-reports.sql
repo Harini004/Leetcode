2108c71db3aec08e36b6ff9c9558ed2b4574767e
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-15 13:32:32 +0200

    .sql Medium 0570-managers-with-at-least-5-direct-reports 2231 ms (5.01%) 0B (100%)

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select name from Employee
-where id in (select managerId from Employee 
-            group by managerId
-            having count(managerId)>=5)
+select name from employee where id in (
+select managerID from employee
+group by managerID having count(*) >=5)
