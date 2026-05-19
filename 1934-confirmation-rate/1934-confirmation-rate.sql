75a864c0d885e7788e2d0d802a7db0cb84599919
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-19 14:21:51 +0200

    .sql Medium 1934-confirmation-rate 633 ms (78.1%) 0B (100%)

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-select s.user_id , Round(Avg(if(c.action='confirmed',1,0)),2) as confirmation_rate
-from confirmations c
-right join Signups s 
-on s.user_id = c.user_id
-group by s.user_id
+select s.user_id,
+round(avg (
+    case 
+    when c.action = 'confirmed' then 1
+else 0 
+end),2) as confirmation_rate
+from confirmations c right join signups s
+on s.user_id = c.user_id group by s.user_id
+
+# Write your MySQL query statement below
