38ded0f4ec5f3925d1d3f5d58bebd2e02afeb155
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-21 15:26:55 +0200

    .sql Easy 1211-queries-quality-and-percentage 362 ms (59.57%) 0B (100%)

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select query_name, round(avg(rating/position),2) as quality,
-round(avg(if(rating<3,1,0)*100),2) as poor_query_percentage
-from queries
-where query_name is not null
+
+select query_name, round(avg(rating/position),2) as quality, round(sum(if(rating<3,1,0))/count(rating)*100,2)
+ as poor_query_percentage from queries
 group by query_name
