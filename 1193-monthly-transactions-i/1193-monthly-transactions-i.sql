f3c8459b854aebb05971ab3cd9c8e3053d782a7c
Harini <45751597+Harini004@users.noreply.github.com>
2026-05-21 15:42:41 +0200

    .sql Medium 1193-monthly-transactions-i 577 ms (66.62%) 0B (100%)

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-select left(trans_date,7) as month, country,
-count(id) as trans_count ,
-sum(state = "approved") as approved_count,
-sum(amount) as trans_total_amount,
-sum((state = "approved")* amount)  as approved_total_amount
-from transactions
-group by month, country
+select concat(year(trans_date),"-",lpad(month(trans_date),2,'0')) as month, country,
+count(id) as trans_count, sum(if(state = 'approved',1,0)) as approved_count, sum(amount) as trans_total_amount, sum(if(state = 'approved',amount,0)) as approved_total_amount
+from transactions group by month, country
